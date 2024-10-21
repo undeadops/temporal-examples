f847a65de442259154b0a4b4970a7cbc4b85b353
Mitch Anderson <mitch@undeadops.xyz>
2024-10-21 14:29:05 -0600

    fix: not working with cron syntax

--- cmd/starter/main.go
+++ cmd/starter/main.go
@@ -33,7 +33,7 @@ func main() {
 	workflowOptions := client.StartWorkflowOptions{
 		ID:           workflowID,
 		TaskQueue:    "cron",
-		CronSchedule: "*/10 * * * *",
+		CronSchedule: "10 * * * *",
 	}
 
 	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, cron.SampleCronWorkflow)
